338bb233cd1b22195570d363b932edb524011ae8
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2026-04-08 13:02:21 +0330

    Working on the breadcrumb

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -139,8 +139,11 @@
                 </div>
                 
                 <!-- Search Input (Centered) -->
-                <div class="hidden md:flex flex-1 max-w-md mx-8 relative z-50 mr-64">
-                    <div class="relative w-full">
+                <div class="hidden md:flex flex-1 max-w-full mx-8 relative z-50 gap-8 justify-between">
+                    <div class="relative w-full max-w-lg" id="navbar-breadcrumb">
+
+                    </div>
+                    <div class="relative w-full max-w-lg">
                         <div class="absolute inset-y-0 {{ $isRtl ? 'right-0 pr-3' : 'left-0 pl-3' }} flex items-center pointer-events-none">
                             <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
